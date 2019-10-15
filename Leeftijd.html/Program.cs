c8cd7a21cbafdb79c5513ae0f51df838faad7fbb
Hagar Van Gucht <hagar.vangucht@student.romerocollege.be>
2019-10-15 09:10:38 +0200

    Add project files.

---
+++ Leeftijd.html/Program.cs
@@ -0,0 +1,94 @@
+﻿//5ITN - 2019-01-04
+//Opgave
+/*======
+ * Vraag naam
+ * vraag geboortedaum 
+ * 
+ * Wat is uw naam?
+ * Wat is uw geboortedaum?
+ * 
+ * 
+ * Analyse
+ * =======
+ * VRAAG naam
+ * Vaag geboortedatum
+ * bereken huidig jaartal(huidige datum .year)
+ * bereken geboortejaar(geboortedatum .year)
+ * Bereken huidige leeftijd
+ * Bereken uitoer (naam, u bent ..."leeftijd jaar oud")
+ * 
+ * 
+ * 
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leeftijd.html
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaraties
+            string naam, geboortedatuminvoer;
+
+            //processingvariabelen
+            DateTime geboortedatum, huidigedatum;
+
+            //outputvariabelen
+            string uitvoer;
+
+
+            //Vraag
+            //VRAAG naam
+            Console.Write("Geef uw naam in, aub :");
+
+
+
+            //Vaag geboortedatum
+            Console.Write("Geef uw gebortedatum aub: ");
+
+            geboortedatuminvoer = Console.ReadLine();
+
+
+
+            //Processing
+            //bereken huidig jaartal(huidige datum.year)
+            //bereken geboortejaar(geboortedatum.year)
+            //Bereken huidige leeftijd
+            //Bereken uitoer(naam, u bent..."leeftijd jaar oud")
+
+
+
+            //output
+            //toon output
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //wachten...
+            Console.WriteLine();
+            Console.Write("Druk op Enter om af te sluiten...");
+            Console.ReadLine();
+
+        }
+    }
+}
